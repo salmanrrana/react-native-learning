f214e56ebdf58f7eca23c5c21b35561e19e10ec6
Salman Rana <salman.r.rana@gmail.com>
2017-06-15 15:32:46 -0500

    react native lesson

---
+++ albums/index.ios.js
@@ -0,0 +1,18 @@
+//Index-ios.js - place code in here for iOS
+
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums!'} />
+    <AlbumList />
+  </View>
+
+  );
+
+//render it to the device
+AppRegistry.registerComponent('albums', () => App);
