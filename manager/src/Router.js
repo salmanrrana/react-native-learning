16fa7e8caa9b0283811cd1f8d432690b26fb4ca9
Salman Rana <salman.r.rana@gmail.com>
2017-06-20 18:16:22 -0500

    can edit employees

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -3,6 +3,7 @@ import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
+import EmployeeEdit from './components/EmployeeEdit';
 
 
 const RouterComponent = () => {
@@ -21,6 +22,7 @@ const RouterComponent = () => {
           onRight={() => Actions.employeeCreate()}
         />
         <Scene key="employeeCreate" component={EmployeeCreate} title="Create an Employee" />
+        <Scene key="employeeEdit" component={EmployeeEdit} title="Edit Emoloyee" />
       </Scene>
     </Router>
   );
